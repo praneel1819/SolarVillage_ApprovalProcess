c5bdc8baca5527e1d1927377fdeff0020c2683b9
pamAdmin <>
2019-03-26 15:06:42 -0400

    {/src/main/java/com/myspace/solarvillage_approvalprocess/ResidentialStructuralPermReq.java}

--- src/main/java/com/myspace/solarvillage_approvalprocess/ResidentialStructuralPermReq.java
+++ src/main/java/com/myspace/solarvillage_approvalprocess/ResidentialStructuralPermReq.java
@@ -6,12 +6,47 @@ package com.myspace.solarvillage_approvalprocess;
 
 public class ResidentialStructuralPermReq implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ResidentialStructuralPermReq() {
-    }
+	@org.kie.api.definition.type.Label(value = "Applicant Address")
+	private java.lang.String applicantAddress;
+	@org.kie.api.definition.type.Label(value = "Applicant Name")
+	private java.lang.String applicantName;
+	@org.kie.api.definition.type.Label(value = "Solar New Order Permit ID")
+	private java.lang.Long solarNewOrderPermitID;
 
+	public ResidentialStructuralPermReq() {
+	}
 
+	public java.lang.String getApplicantAddress() {
+		return this.applicantAddress;
+	}
 
+	public void setApplicantAddress(java.lang.String applicantAddress) {
+		this.applicantAddress = applicantAddress;
+	}
+
+	public java.lang.String getApplicantName() {
+		return this.applicantName;
+	}
+
+	public void setApplicantName(java.lang.String applicantName) {
+		this.applicantName = applicantName;
+	}
+
+	public java.lang.Long getSolarNewOrderPermitID() {
+		return this.solarNewOrderPermitID;
+	}
+
+	public void setSolarNewOrderPermitID(java.lang.Long solarNewOrderPermitID) {
+		this.solarNewOrderPermitID = solarNewOrderPermitID;
+	}
+
+	public ResidentialStructuralPermReq(java.lang.String applicantAddress,
+			java.lang.String applicantName, java.lang.Long solarNewOrderPermitID) {
+		this.applicantAddress = applicantAddress;
+		this.applicantName = applicantName;
+		this.solarNewOrderPermitID = solarNewOrderPermitID;
+	}
 
 }
